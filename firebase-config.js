237922e410af3aacac1ce1a8dee233d5e942fd81
calcappo <03trehmani@gmail.com>
2026-06-22 09:23:42 -0700

    firebase added

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyALcrNCnzemhw_FLGFlWJ0rjM6slpAfAFk",
+  authDomain: "intern-chili-project.firebaseapp.com",
+  databaseURL: "https://intern-chili-project-default-rtdb.firebaseio.com",
+  projectId: "intern-chili-project",
+  storageBucket: "intern-chili-project.firebasestorage.app",
+  messagingSenderId: "383652943705",
+  appId: "1:383652943705:web:beaad8b5e97d6c51089e48",
+  measurementId: "G-11CYR84T4R"
+};
